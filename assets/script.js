b920eaca4889309d0d27bf18a4822c48e70c49a0
christian goddu <rgoddu909@gmail.com>
2023-10-03 18:11:08 -0400

    message

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var mainElement = document.getElementById("main");
-var message = "I'm sorry. You opened up and shared your problems with me. I wanted to help solve them but instead I was an asshole. You are the sweetest and you don't deserve that abuse. I really like hanging out with you. but I'm scared of being vulnerable.  I don't know why I'm like that and I don't know how to fix it. but it makes me sick to think about. ";
+var message = "I'm sorry. You opened up and shared your problems with me. I wanted to help solve them but instead I was an asshole. You are the sweetest and you don't deserve that. I like hanging out with you. but I'm scared of being vulnerable.  I don't know why I'm like that and I don't know how to fix it. but it makes me sick to think about. ";
 var story = message.split(".")
 
 var signing = ["My fault,", "Remi"]
@@ -8,18 +8,38 @@ var signing = ["My fault,", "Remi"]
 function displayMessage(){
     var count = 0
     var msgInterval = setInterval(() => {
-        if (story[count] === undefined){
+        if (count > (story.length - 2)){
             clearInterval(msgInterval)
-            signature()
+            sendImage();
         }
         else{
             mainElement.textContent = story[count];
             count++;
         }
 
-    }, 1700);
+    }, 2200);
+}
+function sendImage(){
+    
+    var count3 = 0
+    var imgInterval = setInterval(() => {
+        
+        if (count3 > 1){
+            clearInterval(imgInterval)
+            signature();
+        }
+        else{
+            var imgEl = document.createElement("img")
+            mainElement.textContent = " ";
+            imgEl.setAttribute("src", "assets/IMG-8930.jpg")
+            mainElement.appendChild(imgEl)
+            count3++;
+        
+        }
+    },1000)
 }
 function signature(){
+    var imgEl = document.createElement("img")
     var count2 = 0
     var sgnInterval = setInterval(() => {
         if (signing[count2] === undefined){

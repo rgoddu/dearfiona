e6197e463513d8cfe6b913306f79ae8a0566af21
Remi G <rgoddu909@gmail.com>
2023-10-06 20:05:20 -0400

    nice

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,57 @@
 var mainElement = document.getElementById("main");
 var welcomeElement = document.getElementById("welcome")
-
-
 var startButton = document.querySelector("button")
+var numberOfMemeImages = 19
+var memeList = []
+
+for(var numberOfMemes = 1; numberOfMemes <= numberOfMemeImages; numberOfMemes++){
+    memeList.push("image" + numberOfMemes)
+}
+console.log(memeList)
+
 startButton.addEventListener("click", () => {
-    sendImage()
+    setTimeout(() => {
+        sendImage()
+    }, 2000);
 })
 
-const fs = require('fs');
-const dir = './assets/img';
+function memeScroll(){
+    var count = 0
+    var 
+}
 
- fs.readdir(dir, (err, files) => {
-  console.log(files.length);
-});
-
-var usedMemes = []
 function sendImage() {
    
-    var count3 = 0
-    var imgInterval = setInterval(() => {
-    var i = Math.floor(Math.random() * 19) + 1
+    // var count3 = 0
+    // var imgInterval = setInterval(() => {
+    var i = Math.floor(Math.random() * memeList.length)
+    console.log("i value" + i)
     
-    
-    console.log(usedMemes)
-        if (count3 > 20) {
+    var imgEl = document.querySelector("#imghere")
+    var meme = "./assets/img/" + memeList[i] + ".jpg"
+    imgEl.setAttribute("src", meme)
+    mainElement.appendChild(imgEl)
+        // if (count3 > 20) {
             welcomeElement.textContent = "why did you click that?";
-            for(var x = 0; x<usedMemes.length; x++)
-
-            localStorage.setItem("memeNumber", i)
-            usedMemes.push(localStorage.getItem("memeNumber"))
-            clearInterval(imgInterval)
+            
+            memeList.splice(i, 1)
+            console.log(memeList.length)
+        if(memeList.length === 0){
+            mainElement.textContent = "no mas"
+            return
+        }
+            // clearInterval(imgInterval)
                 ;
         }
-        else {
-            welcomeElement.textContent = ""
-            welcomeElement.textContent = "wow"
+        // else {
+            // welcomeElement.textContent = ""
+            // welcomeElement.textContent = "wow"
             // var imgEl = document.createElement("img")
-            var imgEl = document.querySelector("#imghere")
-            mainElement.textContent = " ";
-            var meme = "./assets/img/image" + i + ".jpg"
-            imgEl.setAttribute("src", meme)
-            mainElement.appendChild(imgEl)
-            count3++;
+            // count3++;
 
-        }
-    }, 75)
-}
+        // }
+    // }, 75)
+// }
 
 // var message = "I'm sorry. You shared your problems. & I wanted to solve them but instead I was being an asshole. You are the sweetest and you don't deserve that. It makes me sad to think I hurt you. I am a jerk and I think I'm scared of being vulnerable. ";
 // var story = message.split(".")

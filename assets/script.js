1a33cb85ca21053f5cd5f0dbe4551bbd87ab75ce
Remi G <rgoddu909@gmail.com>
2023-10-30 21:05:38 -0400

    new

--- assets/script.js
+++ assets/script.js
@@ -80,39 +80,3 @@ function unstoreMemeList(){
         memeList = checkMemes
     }
 }
-// var message = "I'm sorry. You shared your problems. & I wanted to solve them but instead I was being an asshole. You are the sweetest and you don't deserve that. It makes me sad to think I hurt you. I am a jerk and I think I'm scared of being vulnerable. ";
-// var story = message.split(".")
-
-// var signing = ["My fault,", "Remi" , "also,"]
-
-// function displayMessage() {
-//     welcomeElement.textContent = "Dear Fiona,";
-//     var count = 0
-//     var msgInterval = setInterval(() => {
-//         if (count > (story.length - 2)) {
-//             clearInterval(msgInterval)
-//             signature();
-//         }
-//         else {
-//             mainElement.textContent = story[count];
-            
-//             count++;
-//         }
-
-//     }, 160);
-// }
-
-// function signature() {
-//     var imgEl = document.createElement("img")
-//     var count2 = 0
-//     var sgnInterval = setInterval(() => {
-//         if (signing[count2] === undefined) {
-//             clearInterval(sgnInterval)
-//             sendImage()
-//         }
-//         else {
-//             mainElement.textContent = signing[count2];
-//             count2++
-//         }
-//     }, 1000)
-// }
